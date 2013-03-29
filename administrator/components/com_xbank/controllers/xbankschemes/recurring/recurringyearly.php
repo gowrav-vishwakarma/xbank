8cfafc6d2268a5b8636a01c0274e6fd902d147cc
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-03-29 12:41:52 +0530

    recurring rounded interest

--- administrator/components/com_xbank/controllers/xbankschemes/recurring/recurringyearly.php
+++ administrator/components/com_xbank/controllers/xbankschemes/recurring/recurringyearly.php
@@ -44,11 +44,11 @@ $query = "UPDATE jos_xaccounts as a JOIN jos_xschemes as s on a.schemes_id=s.id 
             $creditAccount = array();
 
             $debitAccount = array(
-                $b->Code . SP . INTEREST_PAID_ON . $schemeName => $totals
+                $b->Code . SP . INTEREST_PAID_ON . $schemeName => round($totals,2)
             );
 
             foreach ($accounts->result() as $acc) {
-                $creditAccount += array($acc->AccountNumber => $acc->CurrentInterest);
+                $creditAccount += array($acc->AccountNumber => round($acc->CurrentInterest,2));
             }
 
             Transaction::doTransaction($debitAccount, $creditAccount, "Interst posting in Recurring Account", TRA_INTEREST_POSTING_IN_RECURRING, Transaction::getNewVoucherNumber(), date("Y-m-d", strtotime(date("Y-m-d", strtotime(getNow("Y-m-d"))) . " -1 day")));
