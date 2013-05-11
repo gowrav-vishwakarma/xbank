d705ff827269daf8ca7a5e625d7b3821a44948d2
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-05-11 11:24:33 +0530

    Create account main Branch and Divison opened and worked on RD committion corrections

--- administrator/components/com_xbank/controllers/test.php
+++ administrator/components/com_xbank/controllers/test.php
@@ -826,7 +826,7 @@ class test extends CI_Controller {
         $paid_before='2013-05-01';
         $transaction_type=10; //RDamountdeposit
 
-
+        $branch= Branch::getCurrentBranch()->id;
 
         $q="SELECT
                 a.id,
@@ -839,7 +839,7 @@ class test extends CI_Controller {
             JOIN jos_xschemes s ON a.schemes_id = s.id
             WHERE
                     t.created_at BETWEEN '$paid_after' AND '$paid_before'
-                AND t.branch_id = 2
+                AND t.branch_id = $branch
                 AND t.transaction_type_id = 10
                 AND s.SchemeType='Recurring'
             GROUP BY
@@ -866,7 +866,68 @@ class test extends CI_Controller {
         foreach ($accounts as $ac) {
             $acc = new Account($ac->id);
             $voucherNo = array('voucherNo' => Transaction::getNewVoucherNumber(), 'referanceAccount' => $ac->id);
-             Premium::setCommissions($acc, $voucherNo,$transactiondate);
+            $this->setCommissions($acc, $voucherNo,$transactiondate);
+        }
+    }
+
+    public function setCommissions($ac, $voucherNo, $transactiondate) {
+        
+
+        $ssddhj = ($ac->id == 5077 ? "sdsds" : "dhdbhj");
+
+        $CI = & get_instance();
+        $amount = $CI->db->query("select SUM(Amount * AgentCommissionPercentage / 100.00 ) AS Totals from jos_xpremiums where Paid <> 0 AND Skipped = 0 AND AgentCommissionSend = 0 AND accounts_id = $ac->id AND Paid <> 0 ")->row()->Totals;
+        $scfcsg = $ac->AccountNumber;
+        $ag = new Agent($ac->agents_id);
+        $agentAccount = $ag->AccountNumber;//get()->AccountNumber;
+        $gbngfn =10;
+        if ($agentAccount) {
+            $agent = new Agent();
+            $agent->where("AccountNumber",$agentAccount)->get();
+            $schemename = $ac->scheme->Name;
+            $agentAccBranch = new Account();
+            $agentAccBranch->where("AccountNumber",$agent->AccountNumber)->get();
+            if ($agentAccBranch->branch_id != Branch::getCurrentBranch()->id) {
+                // INTERBRANCH TRANSECTION
+                $otherbranch = new Branch($agentAccBranch->branch_id);
+
+                $debitAccount = array(
+                    Branch::getCurrentBranch()->Code . SP . COMMISSION_PAID_ON . $schemename => $amount,
+                );
+                $creditAccount = array(
+                    // get agents' account number
+                    //                                            Branch::getCurrentBranch()->Code."_Agent_SA_". $ac->Agents->member_id  => ($amount  - ($amount * 10 /100)),
+                    $otherbranch->Code . SP . BRANCH_AND_DIVISIONS . SP . "for" . SP . Branch::getCurrentBranch()->Code => ($amount - ($amount * TDS_PERCENTAGE / 100)),
+                    Account::getAccountForCurrentBranch(BRANCH_TDS_ACCOUNT)->AccountNumber => ($amount * 10 / 100),
+                );
+                Transaction::doTransaction($debitAccount, $creditAccount, "RD Premium Commission", TRA_PREMIUM_AGENT_COMMISSION_DEPOSIT, $voucherNo, $transactiondate);
+
+                $debitAccount = array(
+                    Branch::getCurrentBranch()->Code . SP . BRANCH_AND_DIVISIONS . SP . "for" . SP . $otherbranch->Code => ($amount - ($amount * TDS_PERCENTAGE / 100)),
+                );
+                $creditAccount = array(
+                    // get agents' account number
+                    $agentAccount => ($amount - ($amount * TDS_PERCENTAGE / 100)),
+                );
+                Transaction::doTransaction($debitAccount, $creditAccount, "RD Premium Commission", TRA_PREMIUM_AGENT_COMMISSION_DEPOSIT, $voucherNo, $transactiondate, $otherbranch->id);
+            } else {
+//                $tdsaccount = Branch::getCurrentBranch()->Code." TDS";
+//                $tdsAcc = Doctrine::getTable("Accounts")->findOneByAccountnumberAndBranch_id($tdsaccount,Branch::getCurrentBranch()->id)->AccountNumber;
+
+                $debitAccount = array(
+                    Branch::getCurrentBranch()->Code . SP . COMMISSION_PAID_ON . $schemename => $amount,
+                );
+                $creditAccount = array(
+                    // get agents' account number
+                    //                                            Branch::getCurrentBranch()->Code."_Agent_SA_". $ac->Agents->member_id  => ($amount  - ($amount * 10 /100)),
+                    $agentAccount => ($amount - ($amount * TDS_PERCENTAGE / 100)),
+                    Account::getAccountForCurrentBranch(BRANCH_TDS_ACCOUNT)->AccountNumber => ($amount * 10 / 100),
+                );
+                Transaction::doTransaction($debitAccount, $creditAccount, "RD Premium Commission", TRA_PREMIUM_AGENT_COMMISSION_DEPOSIT, $voucherNo, $transactiondate);
+            }
+            executeQuery("UPDATE jos_xpremiums SET AgentCommissionSend=1 WHERE Paid <> 0 AND Skipped = 0 AND AgentCommissionSend = 0 AND accounts_id = " . $ac->id);
+//            $AgentSavingAccount=Accounts::getAccountForCurrentBranch(Branch::getCurrentBranch()->Code."_Agent_SA_". $ac->Agents->member_id,false);
+//            Accounts::updateInterest($agentAccount);
         }
     }
 
