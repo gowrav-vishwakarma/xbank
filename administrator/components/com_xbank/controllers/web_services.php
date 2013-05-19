8e2ebd19b3b4c216d83443d8840b491fcf25cfc7
Gowrav Vishwakarma <gowrav@xserver.(none)>
2013-05-19 11:12:25 +0530

    webservices creation started

---
+++ administrator/components/com_xbank/controllers/web_services.php
@@ -0,0 +1,15 @@
+<?php
+
+class web_services extends CI_Controller{
+
+	function web_services(){
+		parent::__construct();
+// 		$this->output->enable_profiler(TRUE);
+	}
+
+	function transaction(){
+		$transactionJSON=inp('transaction_json');
+		echo $transactionJSON;
+	}
+
+}
