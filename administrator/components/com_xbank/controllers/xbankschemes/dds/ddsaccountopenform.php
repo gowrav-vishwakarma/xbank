3a4f556785dadb3573bbe98648d2ace83ca236d7
Gowrav Vishwakarma <gowrav@gowrav-Lenovo-G570.(none)>
2013-12-16 16:34:01 +0530

    Nominee info added in all accounts and account display name, Scheme change facility in edit account (only for default accounts)

--- administrator/components/com_xbank/controllers/xbankschemes/dds/ddsaccountopenform.php
+++ administrator/components/com_xbank/controllers/xbankschemes/dds/ddsaccountopenform.php
@@ -26,6 +26,19 @@ $form = $form->lookupDB("Member ID", "name='UserID' class='input req-string' onb
                 ->div("agentDetailsD", "", $defaultAgent)
                 ->text("DDS amount (in multiples of Rs.300 like 300, 600, 900....3000 etc.)", "name='rdamount' class='input req-string'")// monthly DDS Amount Required
                 ->select("Active Status", "name='ActiveStatus'", array("Active" => '1', "DeActive" => '0'))
+                ->text("Account Name (IF Joint)","name='AccountDisplayName' class='input'")
+                ->_()
+                ->lookupDB("Member ID 2 (For Joint Accounts)", "name='UserID_2' class='input' onblur='javascript:jQuery(\"#memberDetailsS1\").load(\"index.php?option=com_xbank&format=raw&task=accounts_cont.memberDetails&id=\"+this.value);'", "index.php?option=com_xbank&task=accounts_cont.MemberID&format=raw", array("a"=>"b"), array("id", "Name", "FatherName", "BranchName"), "id")
+                ->div("memberDetailsS1", "", $member)
+                ->lookupDB("Member ID 3 (For Joint Accounts)", "name='UserID_3' class='input' onblur='javascript:jQuery(\"#memberDetailsS2\").load(\"index.php?option=com_xbank&format=raw&task=accounts_cont.memberDetails&id=\"+this.value);'", "index.php?option=com_xbank&task=accounts_cont.MemberID&format=raw", array("a"=>"b"), array("id", "Name", "FatherName", "BranchName"), "id")
+                ->div("memberDetailsS2", "", $member)
+                ->lookupDB("Member ID 4 (For Joint Accounts)", "name='UserID_4' class='input' onblur='javascript:jQuery(\"#memberDetailsS3\").load(\"index.php?option=com_xbank&format=raw&task=accounts_cont.memberDetails&id=\"+this.value);'", "index.php?option=com_xbank&task=accounts_cont.MemberID&format=raw", array("a"=>"b"), array("id", "Name", "FatherName", "BranchName"), "id")
+                ->div("memberDetailsS3", "", $member)
+                ->lookupDB("Member ID 5 (For Joint Accounts)", "name='UserID_5' class='input' onblur='javascript:jQuery(\"#memberDetailsS4\").load(\"index.php?option=com_xbank&format=raw&task=accounts_cont.memberDetails&id=\"+this.value);'", "index.php?option=com_xbank&task=accounts_cont.MemberID&format=raw", array("a"=>"b"), array("id", "Name", "FatherName", "BranchName"), "id")
+                ->div("memberDetailsS4", "", $member)
+                ->text('Nominee','name="Nominee" class="input "')
+                ->text('Nominee Age','name="NomineeAge" class="input "')
+                ->text('Relation Nominee','name="RelationWithNominee" class="input "')
                 ->hidden("", "name='CurrentScheme' value='$currentFolder'")
                 ->hidden("", "name='CurrentScheme' value='$currentFolder'");
 
